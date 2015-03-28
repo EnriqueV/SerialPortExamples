2966b400ea12fe515a08f23883dd419a113c5a78
Julio César Rodríguez Domínguez <jurasec@gmail.com>
2015-03-28 01:46:58 -0600

    Se cambia el mensaje default

--- SerialPortExample.js
+++ SerialPortExample.js
@@ -15,9 +15,9 @@ var serialport = new SerialPort("/dev/tty.usbmodem1451",{
 
 this.repl = new Repl({sp: serialport});
 
-function sendCommnad(){;
-  console.log("Enviando comando >>> ", (process.argv[2] || "???"));
-  serialport.write( new Buffer( process.argv[2] || "???") );
+function sendCommnad(){
+  console.log("Enviando comando >>> ", (process.argv[2] || "NodeBots MX"));
+  serialport.write( new Buffer( process.argv[2] || "NodeBots MX") );
 }
 
 serialport.on('open', function(){
@@ -25,3 +25,5 @@ serialport.on('open', function(){
   
   setTimeout( function(){ sendCommnad(); }, 2000);
 });
+
+http://192.168.2.250:9090/docs/PDFDocument.jsp?RowIdPoliza=1-2J7BUF&IdMovimiento=1-2J7BUQ&TipoDeDocumento=Recibo&Folio=RA783954&PAPEL=Bond&Formato=OriginalBond
